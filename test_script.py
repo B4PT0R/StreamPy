83d303e0a3e505f44a8a8a903e988f547f466658
B4PT0R <bferrand.maths@gmail.com>
2023-07-25 20:31:41 +0200

    Added test_script to check if everything works

---
+++ test_script.py
@@ -0,0 +1,59 @@
+
+import numpy as np
+import pandas as pd
+import time
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
+st.button('Click me')
+#st.data_editor('Edit data', data)
+st.checkbox('I agree')
+st.radio('Pick one', ['cats', 'dogs'])
+st.selectbox('Pick one', ['cats', 'dogs'])
+st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
+st.slider('Pick a number', 0, 100)
+st.select_slider('Pick a size', ['S', 'M', 'L'])
+st.text_input('First name')
+st.number_input('Pick a number', 0, 10)
+st.text_area('Text to translate')
+st.date_input('Your birthday')
+st.time_input('Meeting time')
+st.file_uploader('Upload a CSV')
+#st.download_button('Download file', data)
+#st.camera_input("Take a picture")
+st.color_picker('Pick a color')
+#st.image('./header.png')
+#st.audio(data)
+#st.video(data)
+
+col1, col2 = st.columns(2)
+col1.write("This is column 1")
+col2.write("This is column 2")
+
+with st.form(key='my_form'):
+    username = st.text_input('Username')
+    password = st.text_input('Password')
+    st.form_submit_button('Login')
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget.
+st.chat_input("Say something")
+st.balloons()
+time.sleep(2)
+st.snow()
+time.sleep(2)
+st.toast('Warming up...')
+st.error('Error message')
+st.warning('Warning message')
+st.info('Info message')
+st.success('Success message')
+st.exception("Exception")
